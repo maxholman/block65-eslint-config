a8ecc3d373ff5ab4e1893321eb799365a0779950
Max Holman <max@holmn.com>
2020-09-11 16:25:02 +0800

    feat: added formatjs default rules, use react hook recommended defaults

--- react.js
+++ react.js
@@ -1,19 +1,32 @@
 module.exports = {
-  extends: ['./index.js'],
-  plugins: ['eslint-plugin-react', 'react-hooks', 'formatjs'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
+  extends: [
+    './index.js',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+  ],
+  plugins: ['formatjs'],
   rules: {
-    'react/jsx-uses-react': 'error',
-    'react/jsx-uses-vars': 'error',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
+    // 'react/jsx-uses-react': 'error',
+    // 'react/jsx-uses-vars': 'error',
+    'react/prop-types': 0,
     'react/jsx-curly-brace-presence': [
       'warn',
       { props: 'never', children: 'never' },
     ],
-  }
+
+    // Recommended defaults - see https://formatjs.io/docs/tooling/linter/
+    'formatjs/enforce-description': ['error', 'literal'],
+    'formatjs/enforce-default-message': ['error', 'literal'],
+    'formatjs/enforce-placeholders': 'error',
+    'formatjs/no-multiple-whitespaces': 'error',
+    'formatjs/no-multiple-plurals': 'error',
+    'formatjs/enforce-plural-rules': [
+      2,
+      {
+        one: true,
+        other: true,
+        zero: false,
+      },
+    ],
+  },
 };
