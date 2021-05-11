bee0d1ef9f3ed280abdf396e05947040585ebaf0
Max Holman <max@holmn.com>
2021-05-11 16:35:36 +0800

    chore: lint

--- test/fixtures/react/component.tsx
+++ test/fixtures/react/component.tsx
@@ -8,15 +8,14 @@ import {
   useState,
 } from 'react';
 
-export const Link: FC<
-  AnchorHTMLAttributes<HTMLAnchorElement> & { url: URL }
-> = ({ url, children, ...props }) => {
-  return (
-    <a {...props} href={url.toString()}>
-      {children}
-    </a>
-  );
-};
+export const Link: FC<AnchorHTMLAttributes<HTMLAnchorElement> & { url: URL }> =
+  ({ url, children, ...props }) => {
+    return (
+      <a {...props} href={url.toString()}>
+        {children}
+      </a>
+    );
+  };
 
 export const RandomComponent: FC<
   HTMLAttributes<HTMLHeadingElement> & { name: string }
